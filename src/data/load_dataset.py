fefca72677ba6cde638f3b6b8c5c27a013d69460
Jaewon Chung <jaewonc78@gmail.com>
2019-10-18 14:04:33 -0400

    Update load_dataset.py

--- src/data/load_dataset.py
+++ src/data/load_dataset.py
@@ -23,7 +23,7 @@ def load_UMich(ptr=None):
 
 
 def _load_dataset(path, n_nodes, ptr=None):
-    file = np.load(path)
+    file = np.load(path, allow_pickle=True)
     X = file["X"]
     y = file["y"].astype(int)
 
